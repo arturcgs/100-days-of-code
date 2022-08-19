c63542c607267356b589fb4fd0f9ade9c7febac9
Artur Caminero <65780930+arturcgs@users.noreply.github.com>
2022-08-19 09:33:59 -0300

    wrong folders names

--- Projects/Day1 (band name generator)/Day1.py
+++
@@ -1,7 +0,0 @@
-
-print('This program creates awesome band names!')
-
-city = input("That is your home city:\n")
-pet = input("That is your pet's name:\n")
-
-print(f"This could be your band name: {city} {pet}")
--- Projects/README.md
+++
@@ -1,3 +0,0 @@
-# Projects Folders
-
-This folder contains all the folders with every project done in the course!
