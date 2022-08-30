b33bce5211e53e3e93d7cdcce6c4a1208b2b4471
Artur Caminero <65780930+arturcgs@users.noreply.github.com>
2022-08-30 09:30:17 -0300

    Add files via upload

--- Projects/Day 26 (NATO Phonetic Alphabet)/main.py
+++ Projects/Day 26 (NATO Phonetic Alphabet)/main.py
@@ -8,9 +8,8 @@ NATO = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel",
 
 # creating alphabet do NATO dictionary
 alphabet_to_NATO = {letter: nato_word for letter, nato_word in zip(alphabet, NATO)}
-print(alphabet_to_NATO)
 
 # asking user for input
-to_translate = input("What word would you like to translate to NATE Phonetic Alphabet?\n").lower().strip()
-translated_word = [alphabet_to_NATO[letter] for letter in to_translate]
+to_translate = input("What sentence would you like to translate to NATE Phonetic Alphabet?\n").lower().strip()
+translated_word = [alphabet_to_NATO[letter] if letter in alphabet else letter for letter in to_translate]
 print("-".join(translated_word))
