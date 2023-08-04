c684bd462d1db1a211301a44bc452f11464991b1
Artur Caminero Gomes Soares <arturcaminero@gmail.com>
2023-08-04 17:02:03 -0300

    finished day 68

--- Projects/Day 68 (Authentication with Flask)/main.py
+++ Projects/Day 68 (Authentication with Flask)/main.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
-# create flask app and loginmanager
+# create flask app and logi manager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 login_manager = LoginManager()
@@ -21,6 +21,21 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    @staticmethod
+    def is_active(self):
+        return True
+
+    @staticmethod
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.email
  
  
 with app.app_context():
@@ -29,52 +44,79 @@ with app.app_context():
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return db.session.execute(db.select(User).where(User.email == user_id)).scalar()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        # error message for login
+        error = None
         # hashing password
         hashed_password = generate_password_hash(
             password=request.form.get("password"),
             method="pbkdf2:sha256",
             salt_length=8
         )
+        try:
+            # adding new user to db
+            new_user = User(
+                email=new_user_email,
+                password=hashed_password,
+                name=request.form.get("name"),
+            )
+            db.session.add(new_user)
+            db.session.commit()
+        except:
+            # error for email already exists
+            error = "Email already used in an account. Please, login:"
+        print(error)
+        return render_template("login.html", error=error, logged_in=current_user.is_authenticated)
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
-        # adding new user to db
-        new_user = User(
-            email=request.form.get("email"),
-            password=hashed_password,
-            name=request.form.get("name"),
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        return render_template("secrets.html", name=new_user.name)
-    return render_template("register.html")
 
-
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    error = None
+    if request.method == "POST":
+        # getting user object
+        user_id = request.form.get("email")
+        user = db.session.execute(db.select(User).where(User.email == user_id)).scalar()
+        if user:
+            if check_password_hash(pwhash=user.password, password=request.form.get("password")):
+                user.authenticated = True
+                db.session.commit()
+                login_user(user, remember=True)
+                return redirect(url_for("secrets", logged_in=current_user.is_authenticated))
+            else:
+                error = "Wrong password."
+        else:
+            error = "Email not found."
+    return render_template("login.html", error=error, logged_in=current_user.is_authenticated)
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    return render_template("secrets.html", name=current_user.name, logged_in=current_user.is_authenticated)
 
 
 @app.route('/logout')
 def logout():
-    pass
+    user = current_user
+    user.authenticated = False
+    db.session.commit()
+    logout_user()
+    return redirect(url_for("home", logged_in=current_user.is_authenticated))
 
 
 @app.route('/download')
+@login_required
 def download():
     return send_from_directory(
         directory="static", path="files/cheat_sheet.pdf", as_attachment=True
